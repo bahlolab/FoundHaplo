23e24fd42e8cfeb665f894e155e6693a3a37ec3e
Erandee <48813754+Erandee@users.noreply.github.com>
2022-10-03 11:59:48 +1100

    Update create_1000G_control_haplotypes.sh

--- scripts/prepare_inputs/create_1000G_control_haplotypes.sh
+++ scripts/prepare_inputs/create_1000G_control_haplotypes.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 # * Run this once for one DCV* 
-
+set -eu
 MAIN_PATH=$1
 DCV=$2
 CHROMOSOME=$3
-set -eu
+
 module unload R
 module load R/4.2.0 # edit this line accordingly. load the R version with FoundHaplo
 
