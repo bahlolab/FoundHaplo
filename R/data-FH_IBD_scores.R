46447240ab7a3ad23a3abbbeb83ad9c20b050a09
Erandee <robertson.e@wehi.edu.au>
2023-05-15 08:45:33 +1000

    commit

---
+++ R/data-FH_IBD_scores.R
@@ -0,0 +1,10 @@
+#' Example FoundHaplo results for FAME1.chr8.119379052.
+#'
+#' Example FoundHaplo results for FAME1.chr8.119379052. to be used by Analyse_FH.R
+#'
+#' @docType data
+#' @name FH_IBD_scores
+#' @usage data(FH_IBD_scores)
+#' @format ## `FH_IBD_scores`
+#' @keywords datasets
+"FH_IBD_scores"
