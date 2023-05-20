f98030da5a624bc5089619caf22345386c20f1cd
Erandee <48813754+Erandee@users.noreply.github.com>
2023-05-20 15:41:13 +1000

    Update Args_Generate_FH_score.R

--- scripts/run_nextflow/Args_Generate_FH_score.R
+++ scripts/run_nextflow/Args_Generate_FH_score.R
@@ -14,25 +14,29 @@ db_name=args[5]
 db_unix_socket=args[6]
 DCV=args[7]
 minor_allele_cutoff=args[8]
-imputation_quality_score_cutoff_test=args[9]
-frequency_type=args[10]
-geneticMap_DIR=args[11]
-disease_files_DIR=args[12]
-test_file=args[13]
-test_name=args[14]
-test_list=args[15]
-data_type=args[16]
-controls_file_DIR=args[17]
-save_report_DIR=args[18]
-TEMP_DIR=args[19]
+gen_allele_mismatch_rate=args[9]
+MA_cutoff=args[10]
+meiosis=args[11]
+
+imputation_quality_score_cutoff_test=args[12]
+frequency_type=args[13]
+geneticMap_DIR=args[14]
+disease_files_DIR=args[15]
+test_file=args[16]
+test_name=args[17]
+test_list=args[18]
+data_type=args[19]
+controls_file_DIR=args[20]
+save_report_DIR=args[21]
+TEMP_DIR=args[22]
+
+
 
 
 class(imputation_quality_score_cutoff_test)="numeric"
 class(minor_allele_cutoff)="numeric"
-class(db_port)="numeric
 ###check below all the time
 
 library(FoundHaplo)
 
-Generate_FH_score(source_of_disease_haplotypes,db_port,db_host,db_password,db_name,db_unix_socket,DCV,minor_allele_cutoff=0,imputation_quality_score_cutoff_test,frequency_type,geneticMap_DIR,disease_files_DIR,test_file,test_name,test_list,data_type,controls_file_DIR,save_report_DIR,TEMP_DIR)
-
+Generate_FH_score(source_of_disease_haplotypes,db_port,db_host,db_password,db_name,db_unix_socket,DCV,minor_allele_cutoff,gen_allele_mismatch_rate,MA_cutoff,meiosis,imputation_quality_score_cutoff_test,frequency_type,geneticMap_DIR,disease_files_DIR,test_file,test_nam,test_list,data_type,controls_file_DIR,save_report_DIR,TEMP_DIR)
