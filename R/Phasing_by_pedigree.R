8c2543f28a2f9bc28a1cb1349ce7568fb1b9a465
Erandee <robertson.e@wehi.edu.au>
2023-10-11 11:39:07 +1100

    commit

--- R/Phasing_by_pedigree.R
+++ R/Phasing_by_pedigree.R
@@ -158,7 +158,7 @@ Phasing_by_pedigree=function(input_vcf,output_DIR,sample_info_file,n.cores)
       colnames(dummy_sum)=c("sample_1","sample_2")
       haplotype_file_1=data.frame(cbind(haplotype_file_1,dummy_sum))
       # remove markers that can not be distinguished
-      haplotype_file_1=haplotype_file_1[which(haplotype_file_1$affected.o!=1 | haplotype_file_1$affected.p!=1|haplotype_file_1$unaffected.p!=1),]
+      haplotype_file_1=haplotype_file_1[which(haplotype_file_1$sample_1!=1 | haplotype_file_1$sample_2!=1),]
 
       haplotype_file_2 = haplotype_file_1[,!(names(haplotype_file_1) %in% c("sample_1","sample_2"))]
 
